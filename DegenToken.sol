3b31cb8f9115083376fc98c474c4f729a6d278d6
hardikj2003 <68813211+hardikj2003@users.noreply.github.com>
2023-07-14 23:27:17 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,1 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
+
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+
+    constructor() ERC20("Degen", "DGN") {}
+
+        function mint(address to, uint256 amount) public onlyOwner{
+            _mint(to, amount);
+        }
+        function transferTokens(address _reciever, uint amount) external{
+            require(balanceOf(msg.sender) >= amount, "you are not owner");
+            approve(msg.sender, amount);
+            transferFrom(msg.sender, _reciever, amount);
+        }
+        function checkBalance() external view returns(uint){
+           return balanceOf(msg.sender);
+        }
+        function burnTokens(uint amount) external{
+            require(balanceOf(msg.sender)>= amount, "You do not have enough Tokens");
+            _burn(msg.sender, amount);
+        }
+        function RedemptionItems() public pure returns(string memory) {
+            return "1. SuperPlayer NFT value = 350 \n 2. CaptainKing value = 150 /n 3. DGNStar value = 75";
+        }
+        function reedemTokens(uint Item_id) external payable{
+            require(Item_id<=3,"Invalid selection");
+            if(Item_id ==1){
+                require(balanceOf(msg.sender)>=200, "Insufficient Balance");
+                approve(msg.sender, 200);
+                transferFrom(msg.sender, owner(), 200);
+            }
+            else if(Item_id ==2){
+                require(balanceOf(msg.sender)>=100, "Insufficient Balance");
+                approve(msg.sender, 100);
+                transferFrom(msg.sender, owner(), 100);
+            }
+            else{
+                require(balanceOf(msg.sender)>=75, "Insufficient Balance");
+                approve(msg.sender, 75);
+                transferFrom(msg.sender, owner(), 75);
+            }
+
+
+        }
+
+}
